02c410a4f94ea7a232da102858e2c606f15a6cd9
Jill Oakes <jill.oakes@beckshybrids.com>
2022-07-05 12:00:12 -0400

    Complete module 6.2

---
+++ CityInfo.API/Entities/City.cs
@@ -0,0 +1,16 @@
+﻿namespace CityInfo.API.Entities
+{
+    public class City
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+
+        public ICollection<PointOfInterest> PointsOfInterest { get; set; } = new List<PointOfInterest>();
+
+        public City(string name)
+        {
+            Name = name;
+        }
+    }
+}
