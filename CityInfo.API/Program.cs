bfb951b700f987980be304d27a143f24d48c464b
Jill Oakes <jill.oakes@beckshybrids.com>
2022-07-06 12:02:18 -0400

    Complete Module 7

--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -40,6 +40,10 @@ builder.Services.AddSingleton<CitiesDataStore>();
 builder.Services.AddDbContext<CityInfoContext>(dbContextOptions => dbContextOptions.UseSqlite(
     builder.Configuration["ConnectionStrings:CityInfoDBConnectionString"]));
 
+builder.Services.AddScoped<ICityInfoRepository, CityInfoRepository>();
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
